2345e25a623d11454c71cbab8dd54dbfbd9facfe
skiboorg <d@skib.org>
2021-12-19 17:09:37 +0300

    fixes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -63,74 +63,74 @@
       </div>
     </section>
 
-    <section ref="feedback" id="feedback" class="feedback">
-      <p class="text-center text-secondary text-h3 section-title">Избранные отзывы</p>
-       <q-carousel
-      v-model="slide"
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      swipeable
-      animated
-      control-color="primary"
-      padding
+<!--    <section ref="feedback" id="feedback" class="feedback">-->
+<!--      <p class="text-center text-secondary text-h3 section-title">Избранные отзывы</p>-->
+<!--       <q-carousel-->
+<!--      v-model="slide"-->
+<!--      transition-prev="slide-right"-->
+<!--      transition-next="slide-left"-->
+<!--      swipeable-->
+<!--      animated-->
+<!--      control-color="primary"-->
+<!--      padding-->
 
-      class="bg-primary fit-content"
-    >
-      <q-carousel-slide :name="1" class="column no-wrap ">
-        <div class="row q-col-gutter-lg ">
-          <div class="col-12 col-md-4 feedback-item">
-            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>
-            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>
-            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>
-          </div>
-            <div class="col-12 col-md-4 feedback-item">
-            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>
-            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>
-            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>
-          </div>
-             <div class="col-12 col-md-4 feedback-item">
-            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>
-            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>
-            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>
-          </div>
-        </div>
-      </q-carousel-slide>
-           <q-carousel-slide :name="2" class="column no-wrap ">
-        <div class="row q-col-gutter-lg ">
-          <div class="col-12 col-md-4 feedback-item">
-            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>
-            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>
-            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>
-          </div>
-            <div class="col-12 col-md-4 feedback-item">
-            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>
-            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>
-            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>
-          </div>
-             <div class="col-12 col-md-4 feedback-item">
-            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>
-            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>
-            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>
-          </div>
-        </div>
-      </q-carousel-slide>
+<!--      class="bg-primary fit-content"-->
+<!--    >-->
+<!--      <q-carousel-slide :name="1" class="column no-wrap ">-->
+<!--        <div class="row q-col-gutter-lg ">-->
+<!--          <div class="col-12 col-md-4 feedback-item">-->
+<!--            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>-->
+<!--            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>-->
+<!--            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>-->
+<!--          </div>-->
+<!--            <div class="col-12 col-md-4 feedback-item">-->
+<!--            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>-->
+<!--            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>-->
+<!--            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>-->
+<!--          </div>-->
+<!--             <div class="col-12 col-md-4 feedback-item">-->
+<!--            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>-->
+<!--            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>-->
+<!--            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </q-carousel-slide>-->
+<!--           <q-carousel-slide :name="2" class="column no-wrap ">-->
+<!--        <div class="row q-col-gutter-lg ">-->
+<!--          <div class="col-12 col-md-4 feedback-item">-->
+<!--            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>-->
+<!--            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>-->
+<!--            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>-->
+<!--          </div>-->
+<!--            <div class="col-12 col-md-4 feedback-item">-->
+<!--            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>-->
+<!--            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>-->
+<!--            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>-->
+<!--          </div>-->
+<!--             <div class="col-12 col-md-4 feedback-item">-->
+<!--            <q-icon class="q-mb-lg" color="white" size="50px" name="format_quote"/>-->
+<!--            <p class="text-bond text-h5">Продовольственный онлайн-центр</p>-->
+<!--            <p class="text-bond text-body1">Рекомендации — это короткие цитаты от людей,которые любят ваш бренд. Это отличный способубедить клиентов попробовать ваши услуги.</p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </q-carousel-slide>-->
 
 
-    </q-carousel>
-    </section>
+<!--    </q-carousel>-->
+<!--    </section>-->
     <section class="contacts bg-secondary">
       <div class="row  items-center">
         <div class="col-12 col-md-6">
           <q-img  src="~assets/contacts-img.png"/>
         </div>
         <div class="col-12 col-md-6 contacts-bg q-pl-md q-pl-md-xl q-py-lg">
-           <p class=" text-h3 text-primary  text-lh-100">Свяжитесь с нами</p>
-          <p class="text-bond text-h6 q-mb-none">ЭЛЕКТРОННЫЙ АДРЕС</p>
-          <p> <a class="text-bond text-h6" href="mailto:privyet@website.ru">privyet@website.ru</a></p>
- <p class="text-bond text-h6 q-mb-none">ПОЧТОВЫЙ АДРЕС</p>
-          <p class="text-bond text-h6"> ул.Первомайская, д.123, индекс 123321</p>
-           <p class="text-bond text-h6 q-mb-none">ТЕЛЕФОННЫЙ НОМЕР</p>
-          <p> <a class="text-bond text-h6" href="tel:+74994697890">+7(499) 469-78-90</a></p>
+<!--           <p class=" text-h3 text-primary  text-lh-100">Свяжитесь с нами</p>-->
+<!--          <p class="text-bond text-h6 q-mb-none">ЭЛЕКТРОННЫЙ АДРЕС</p>-->
+<!--          <p> <a class="text-bond text-h6" href="mailto:privyet@website.ru">privyet@website.ru</a></p>-->
+<!-- <p class="text-bond text-h6 q-mb-none">ПОЧТОВЫЙ АДРЕС</p>-->
+<!--          <p class="text-bond text-h6"> ул.Первомайская, д.123, индекс 123321</p>-->
+<!--           <p class="text-bond text-h6 q-mb-none">ТЕЛЕФОННЫЙ НОМЕР</p>-->
+<!--          <p> <a class="text-bond text-h6" href="tel:+74994697890">+7(499) 469-78-90</a></p>-->
         </div>
       </div>
     </section>
@@ -158,36 +158,36 @@
         <img draggable="false" src="~assets/philosophy-img.png">
       </div>
     </section>
-    <section ref="faq" id="faq" class="faq">
-       <p class="text-center text-primary text-h3 ">Ответы на вопросы</p>
-      <div class="text-center">
-         <img draggable="false" src="~assets/logo.png">
-      </div>
-      <div class="faq-wrapper">
-          <q-list class="q-mb-lg">
-      <q-expansion-item
-        group="faqgroup"
-        :label="item.q"
-        header-class="text-primary text-bold"
-        v-for="(item,index) in faq"
-        :default-opened="index===0"
-        :key="index"
-        class="bg-grey-3 q-mb-md"
-      >
-        <q-card class="bg-grey-2">
-          <q-card-section>
-           {{item.a}}
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
+<!--    <section ref="faq" id="faq" class="faq">-->
+<!--       <p class="text-center text-primary text-h3 ">Ответы на вопросы</p>-->
+<!--      <div class="text-center">-->
+<!--         <img draggable="false" src="~assets/logo.png">-->
+<!--      </div>-->
+<!--      <div class="faq-wrapper">-->
+<!--          <q-list class="q-mb-lg">-->
+<!--      <q-expansion-item-->
+<!--        group="faqgroup"-->
+<!--        :label="item.q"-->
+<!--        header-class="text-primary text-bold"-->
+<!--        v-for="(item,index) in faq"-->
+<!--        :default-opened="index===0"-->
+<!--        :key="index"-->
+<!--        class="bg-grey-3 q-mb-md"-->
+<!--      >-->
+<!--        <q-card class="bg-grey-2">-->
+<!--          <q-card-section>-->
+<!--           {{item.a}}-->
+<!--          </q-card-section>-->
+<!--        </q-card>-->
+<!--      </q-expansion-item>-->
 
 
-    </q-list>
-         <div class="text-center">
-         <img draggable="false" src="~assets/faq-img.png">
-      </div>
-      </div>
-    </section>
+<!--    </q-list>-->
+<!--         <div class="text-center">-->
+<!--         <img draggable="false" src="~assets/faq-img.png">-->
+<!--      </div>-->
+<!--      </div>-->
+<!--    </section>-->
 
   </q-page>
 </template>
